347448c10f7e1b806cb7d3c26ca2eadcada81c13
zklovekissxx <votrungquan.tp15@gmail.com>
2021-10-23 14:27:58 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('Hi! Java script bacsic');
+
+console.log('Đây là 1 dòng log');
+prompt('Xác Nhận bạn đủ tuổi');
+
+
+    
+var number = 10
+
+var a = ++number * 3 - number-- * 2
+var b = number++ * 2 - --number * 3
+
+console.log(a + b)
